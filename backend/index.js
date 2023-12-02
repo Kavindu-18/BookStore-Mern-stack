0eb88bb1b0adb49d7e17ba16f3589ce4275e35df
Kavindu-18 <kavinduperera19@gmail.com>
2023-12-02 19:32:22 +0530

    api

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ import { Book } from './models/bookModel.js';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send('Hello World!');
@@ -21,12 +23,55 @@ app.post('/books',async (request, response) => {
             return response.status(400).send({message: 'Data fields missing'});
         }
 
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
+
+        const book = await Book.create(newBook);
+
+        return response.status(201).send(book);
+
     }
         
      catch (error) {
         console.log(error.massage);
         response.status(500).send({message: error.message});
 
+    }
+});
+
+//routing to get all books from database
+
+app.get('/books', async (request, response) => {
+    try{
+        const books = await Book.find({});
+        return response.status(200).send(books);
+
+        count:books.length;
+        data:books;
+
+    }catch(error){
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
+//routing to get all books from database by id
+
+app.get('/books/:id', async (request, response) => {
+    try{
+
+        const { id } = request.params;
+
+        const book = await Book.findby(id);
+
+        return response.status(200).json(book);
+
+    }catch(error){
+        console.log(error.message);
+        response.status(500).send({message: error.message});
     }
 });
 
